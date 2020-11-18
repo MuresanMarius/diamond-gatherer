21927df2132e013f405dd7edf4e170abc027c7a5
MariusMuresan <mariusflaviumuresan@gmail.com>
2020-11-18 12:57:47 +0200

    Tema 1

---
+++ tema 1/pagina.js
@@ -0,0 +1,15 @@
+var person={
+Name: "Marius",
+Age: "18",
+Profession: "student",
+eyeColor: "green",
+height: "1.77",
+weigh: "68"
+
+}
+console.log(person)
+
+function myFunction(p1, p2) {
+    p1="Buna, numele meu este";
+    p2="Marius" ;
+    return p1 + p2;}
